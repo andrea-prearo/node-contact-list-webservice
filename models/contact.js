6dcf4fdecf9f8c2a4d854f621aaabf872c374f76
Andrea Prearo <andrea.prearo@gmail.com>
2016-03-19 21:38:27 -0700

    Update Contact schema

--- models/contact.js
+++ models/contact.js
@@ -2,24 +2,30 @@ var mongoose = require('mongoose');
 
 // Contact schema
 var ContactSchema = new mongoose.Schema({
-    avatar: String,
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true        
-    },
-    company: String,
-    phone: [String],
-    email: [String],
-    location: [{
-      address: String,
-      city: String,
-      state: String,
-      country: String,
-      zipCode: String
+  avatar: String,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true        
+  },
+  company: String,
+  phone: [{
+    label: String,
+    number: String
+  }],
+  email: [{
+    label: String,
+    address: String
+  }],
+  location: [{
+    address: String,
+    city: String,
+    state: String,
+    country: String,
+    zipCode: String
   }]
 });
 
